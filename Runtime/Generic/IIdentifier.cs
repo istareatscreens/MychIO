f856bb411a893ea3392c91b8fb8a2afc3befdcfc
istareatscreens <s.charb@gmail.com>
2024-11-13 20:48:00 -0500

    Fix Id property get to prevent stack overflow instead throw not implemented exception

--- Runtime/Generic/IIdentifier.cs
+++ Runtime/Generic/IIdentifier.cs
@@ -1,10 +1,19 @@
+using System;
+
 namespace MychIO.Generic
 {
+    /* 
+     TODO: Use this property instead of device classification to support duplicate devices (multiplayer support)
+        - Updating IOManager Dictionaries to map by classification and deviceId.
+        - Updating IOEvents to include deviceID
+        - Updating Device.CanConnect method to support multiple devices in check (prevent conflicting properties with other devices)
+        - Update IOManager methods to use device Id when interacting with an individual device
+    */
     public interface IIdentifier
     {
         public string Id
         {
-            get => Id;
+            get => throw new NotImplementedException($"Identification getter not implemented for device {GetType().FullName}");
         }
 
     }
